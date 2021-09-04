de59560405cc328b534d01c6c21b66408f3ee457
Al Martinez <al@sitparker.com>
2021-09-04 15:40:25 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => {
+  return(
+    <div>
+      <h1>Hats</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.componet.jsx
+++ src/components/directory/directory.componet.jsx
@@ -47,8 +47,8 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size})=> {
-                         return <MenuItem key={id} imageUrl={imageUrl} title={title} size={size}/>
+                    this.state.sections.map(({ id, ...otherSectionProps})=> {
+                         return <MenuItem key={id} {...otherSectionProps}/>
                     })
                 }
             </div>
