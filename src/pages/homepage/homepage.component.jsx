6e097f95370b160ea4ccc5fbd9f1ed4eb7635da8
Al Martinez <al@sitparker.com>
2021-10-10 15:08:53 -0400

    cart with redux

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.componet';
 import './homepage.styles.scss';
+
 const HomePage = () => (
     <div className='homepage'>
         <Directory/>
